04358ec4f31d01dc41788a1ea0f12fee04175ecc
LuckyCodesss <fifagramers@gmail.com>
2022-11-18 16:27:02 +0700

    ch

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const Community = require("./models/communityModel");
 const bodyParser = require('body-parser');
 
 mongoose.connect(
-    'mongodb+srv://WebHouse:max2666z!@cluster0.a6krzpb.mongodb.net/fifa?retryWrites=true&w=majority',
+    'mongodb+srv://WebHouse:max2666z!@cluster0.a6krzpb.mongodb.net/server5?retryWrites=true&w=majority',
     { useNewUrlParser: true }
 )
 
 app.set('view engine','ejs')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 
-app.post('/user',async (req,res)=>{
+app.post('/users',async (req,res)=>{
     const newUser = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -27,29 +27,67 @@ app.post('/user',async (req,res)=>{
         bio: req.body.bio,
         project: req.body.project
     }
-    console.log(newUser)
-    res.render("index",{project:newUser})
+    console.log("okay")
+    res.status(201).send('Create user')
+})
+app.post('/project',async (req,res)=>{
+    const newProject = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        email: req.body.email,
+        bio: req.body.bio,
+        project: req.body.project
+    }
+    console.log("okay")
+    res.status(201).send('Create project')
+})
+app.post('/tools',async (req,res)=>{
+    const newTool = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        email: req.body.email,
+        bio: req.body.bio,
+        project: req.body.project
+    }
+    console.log("okay")
+    res.status(201).send('Create tool')
+})
+app.post('/communitys',async (req,res)=>{
+    const newCommunity = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        email: req.body.email,
+        bio: req.body.bio,
+        project: req.body.project
+    }
+    console.log("okay")
+    res.status(201).send('Create community')
 })
 
 
 
 //get data
-app.get('/user',async (req,res)=>{
+app.get('/users',async (req,res) => { 
     var data = await User.find({})
     res.render("index",{project:data})
 })
-app.get('/project',async (req,res)=>{
+app.get('/projects',async (req,res) => {
     var data = await Project.find({})
     res.render("index",{project:data})
 })
-app.get('/tool',async (req,res)=>{
+app.get('/tools',async (req,res) => {
     var data = await Tool.find({})
     res.render("index",{project:data})
 })
-app.get('/community',async (req,res)=>{
+app.get('/communitys',async (req,res) => {
     var data = await Community.find({})
     res.render("index",{project:data})
 })
+
+
 
 app.listen(9999);
 app.use((req, res) => {
